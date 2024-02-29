0b7943e9c2a852134bd9d6a2563b2478bae53d9f
Darshan Parmar <darshan.optimized@gmail.com>
2024-02-29 17:55:13 +0530

    changes

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, "public/profilePicture/b&w")));
 const cors = require("cors");
 app.use(cors());
 
-app.use("/", indexRouter);
+// app.use("/", indexRouter);
 app.use("/users/v1", usersRouter);
 app.use("/image/v1", imageRouter);
 app.use("/states/v1", stateRouter);
